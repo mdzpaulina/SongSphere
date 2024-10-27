2b15792eec4c421c14f766f897b6940466394390
Gcumplido59 <gaelmendo59@gmail.com>
2024-10-27 15:57:52 -0600

    created .css .jsx Of home page

--- songsphere/src/home.jsx
+++ songsphere/src/home.jsx
@@ -1,3 +1,12 @@
 import React from 'react';
 import './home.css';
 
+function home(){
+    return(
+        <div>
+            <head>
+                Hola
+            </head>
+        </div>
+    )
+}
